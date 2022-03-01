bc4b150ac06883f4e261154f4b466145be9eda88
NateyBoyDM <nathangwunschl@gmail.com>
2022-03-01 08:35:42 -0500

    Added Polybar Config Files

---
+++ .config/polybar/info-optimus-manager.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+# Simple script for displaying and switching gpus for optimus-manager and polybar.
+
+
+# Configuration
+
+# Set the symbols to be displayed in polybar
+intel_symbol="GPU: Intel"
+nvidia_symbol="GPU: Nvidia"
+hybrid_symbol="GPU: Hybrid"
+
+# Prefered setup. 
+# Set to 1 if the system switches between hybrid and nvidia.
+# Set to 0 if the system switches between intel and nvidia.
+hybrid_switching=0
+
+
+
+# Functions
+
+get_current(){
+	mode=$(prime-offload && optimus-manager --print-mode)
+	
+	if [[ $mode == *"integrated"* ]]; then
+		echo "integrated"
+	elif [[ $mode == *"nvidia"* ]]; then
+		echo "nvidia"
+	elif [[ $mode == *"hybrid"* ]]; then
+		echo "hybrid"
+	fi
+}
+
+display_gpu(){
+	if [[ $(get_current) == "integrated" ]]; then
+		echo "$intel_symbol"
+	elif [[ $(get_current) == "nvidia" ]]; then
+		echo "$nvidia_symbol"
+	elif [[ $(get_current) == "hybrid" ]]; then
+		echo "$hybrid_symbol"
+	fi
+}
+
+switch_gpu(){	
+	if [[ $(get_current) == "integrated" ]]; then
+		next="nvidia"
+	elif [[ $(get_current) == "nvidia" ]]; then
+		if [[ $hybrid_switching == 1 ]]; then
+			next="hybrid"
+		else
+			next="intel"
+		fi
+	elif [[ $(get_current) == "hybrid" ]]; then
+		next="nvidia"
+	fi	
+	eval "prime-offload && optimus-manager --switch $next --no-confirm"
+}
+
+# Handle arguments. 
+
+case "$1" in
+	--switch)
+	switch_gpu
+	;;
+*)
+	display_gpu
+	;;
+esac
+
+
